d92beb1592e0d2dc6eb14008a73151c2522db150
Hamdy ahmed <hamdyeng494@gmail.com>
2020-04-22 07:59:12 -0400

    initial commit

---
+++ tests/feature/TestTesting.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Tests\Feature;
+
+
+use Orchestra\Testbench\TestCase;
+
+class TestTesting extends TestCase
+{
+    public function test()
+    {
+        return $this->assertEquals();
+    }
+
+}
